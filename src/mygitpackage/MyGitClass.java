cb47fed877b1827d4f9ef0052267db6731bdab86
ufuktakmak <ufuktakmak@gmail.com>
2021-04-04 16:56:15 +0200

    mesaj

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,6 +5,8 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("my second commit");
+		System.out.println("my second committt");
+		System.out.println("my other committt");
 	}
 
 }
